b3a9e5f7e048182ac44268babc67613a4edb2127
Vigneshsai <vigneshsaikasarla@gmail.com>
2026-03-29 02:07:38 +0530

    fix cors config

--- backend/server/src/main/java/com/example/server/config/CorsConfig.java
+++ backend/server/src/main/java/com/example/server/config/CorsConfig.java
@@ -1,15 +1,14 @@
 package com.example.server.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return registry -> registry.addMapping("/**")
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
                 .allowedOrigins("https://vigneshsaia29.vercel.app")
                 .allowedMethods("*");
     }
