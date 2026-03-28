25704ace5a47a5eef72569ab457212c8da16dc0d
Vigneshsai <vigneshsaikasarla@gmail.com>
2026-03-29 01:38:35 +0530

    connect frontend to backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8081/api"
+  baseURL: "https://smart-petrol-booking.onrender.com"
 });
 
 // Add a request interceptor to include the JWT token in headers
